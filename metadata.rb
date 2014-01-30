28689d57b83ae191e9dd698bd113bdcdb581071d
Eric Herot <eric.github@herot.com>
2014-01-30 11:24:30 -0500

    Add missing known_host definition

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures chefgithook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 depends 'runit', '~> 1.5.8'
 depends 's3_file', '~> 2.3.0'
