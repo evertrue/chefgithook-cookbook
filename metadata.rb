d631839f1e15d4a52568d240e7bd854d999d45ae
Eric Herot <eric.github@herot.com>
2015-02-24 18:03:29 -0500

    Version bump 1.0.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures chefgithook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.4'
+version          '1.0.5'
 
 depends 'runit', '~> 1.5.8'
 depends 's3_file', '~> 2.3.0'
