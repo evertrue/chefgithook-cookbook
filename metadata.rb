9bb52bb18ccc5aa5d2da2ad61c253aa5bb44b274
Eric Herot <eric.github@herot.com>
2015-04-07 15:50:29 -0400

    Update the license type

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'chefgithook'
 maintainer       'EverTrue, Inc.'
 maintainer_email 'eric.herot@evertrue.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures chefgithook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
