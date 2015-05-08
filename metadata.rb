e7ca276e4d1c087a3bc44aa8cf32908938132f05
Eric Herot <eric.github@herot.com>
2015-05-08 17:33:29 -0400

    Include source and issues URL in metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures chefgithook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.2.0'
+source_url       'https://github.com/evertrue/chefgithook-cookbook'
+issues_url       'https://github.com/evertrue/chefgithook-cookbook/issues'
 
 depends 'runit', '~> 1.5'
 depends 's3_file', '~> 2.5'
