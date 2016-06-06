1dc864c24b971c1a0a31308f034b835f990e80e6
Eric Herot <eric.github@herot.com>
2016-06-06 15:18:19 -0400

    Version bump 3.1.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures chefgithook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.1.4'
+version          '3.1.5'
 source_url       'https://github.com/evertrue/chefgithook-cookbook'
 issues_url       'https://github.com/evertrue/chefgithook-cookbook/issues'
 
